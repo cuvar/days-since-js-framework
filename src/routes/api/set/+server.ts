d79cdc1e6c8ab09d2b7f951452b662d5fc4686b6
cuvar <luca-mueller@gmx.net>
2022-11-24 16:03:46 +0100

    feat: add token auth

--- src/routes/api/set/+server.ts
+++ src/routes/api/set/+server.ts
@@ -1,6 +1,7 @@
 import { error } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import redis from "../../../../src/util/redis";
+import { POST_TOKEN } from "$env/static/private";
 
 const dateRegex = new RegExp("2[0-9]{3}-(0[1-9]|10|11|12)-(0[1-9]|10|11|12)");
 
@@ -9,7 +10,13 @@ export const POST: RequestHandler = async ({ request }) => {
   const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
 
   let dateToUpdate = "";
-  const { date } = await request.json();
+  const { date, token } = await request.json();
+
+  if (token !== POST_TOKEN) {
+    return new Response(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+    });
+  }
 
   if (typeof date !== "string" || !dateRegex.test(date)) {
     dateToUpdate = today;
