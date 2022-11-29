62c2c4517648a849ecdc50022d98c7d76b3465e8
cuvar <luca-mueller@gmx.net>
2022-11-29 10:54:35 +0100

    feat: add github info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,16 @@
   />
 </svelte:head>
 <div class="font-mono w-screen h-screen">
+  <nav class="absolute top-0 w-full">
+    <div class="pt-8 px-8 text-right">
+      <a
+        href={"https://github.com/cuvar/days-since-js-framework"}
+        class="hover:underline text-blue-800"
+        target="_blank"
+        rel="noreferrer noopenener">GitHub</a
+      >
+    </div>
+  </nav>
   <main class="w-full h-full flex justify-center items-center">
     {#if data.status == 500}
       <div>An error occurred</div>
@@ -22,6 +32,14 @@
     {/if}
   </main>
   <footer class="text-sm text-center py-8 absolute bottom-0 w-full px-4">
-    You think the data is wrong? Send me an email: info (at) cuvar.dev
+    <div>
+      You think the data is wrong? Send me an email: info (at) cuvar.dev or file
+      an issue <a
+        href={"https://github.com/cuvar/days-since-js-framework/issues"}
+        class="hover:underline text-blue-800"
+        target="_blank"
+        rel="noreferrer noopenener">here</a
+      >!
+    </div>
   </footer>
 </div>
