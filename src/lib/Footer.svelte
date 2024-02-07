2a921fc0d276d8c6b5e9d2d33539e8d806fa0c6b
cuvar <luca@cuvar.dev>
2024-02-07 18:26:52 +0100

    fix: footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,6 @@
 <footer class="text-sm text-center py-8 w-full px-4">
   <div>
-    You think the data is wrong? Send me an email: luca (at) cuvar.dev or file
-    an issue <a
+    You think the data is wrong? File an issue <a
       href={"https://github.com/cuvar/days-since-js-framework/issues"}
       class="hover:underline text-blue-800"
       target="_blank"
