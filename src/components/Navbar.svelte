334eceb3adbccff712f7abde72608c1f2456d24c
cuvar <luca@cuvar.dev>
2024-08-18 10:57:32 +0200

    feat: move files

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<nav class="w-full flex justify-between">
+  <div class="pt-8 px-8 text-left space-x-4">
+    <a
+      href={"/"}
+      class="hover:underline text-blue-800"
+      rel="noreferrer noopenener">Home</a
+    >
+  </div>
+  <div class="pt-8 px-8 text-right space-x-4">
+    <a
+      href={"/history"}
+      class="hover:underline text-blue-800"
+      rel="noreferrer noopenener">History</a
+    >
+    <a
+      href={"https://github.com/cuvar/days-since-js-framework"}
+      class="hover:underline text-blue-800"
+      target="_blank"
+      rel="noreferrer noopenener">GitHub</a
+    >
+  </div>
+</nav>
