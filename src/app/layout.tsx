0680d5941d2c5846bda2e9af258ae45d1ff89d93
cuvar <luca@cuvar.dev>
2024-08-18 11:24:26 +0200

    feat: counter page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
+import Footer from "~/components/Footer";
+import Navbar from "~/components/Navbar";
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
@@ -16,8 +18,17 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+      <body className="h-full min-h-screen w-screen font-mono">
+        <TRPCReactProvider>
+          <Navbar />
+          <main className="h-full w-full py-10">{children}</main>
+          <Footer />
+        </TRPCReactProvider>
+        <script
+          defer
+          data-domain="dayssincelastjsframework.com"
+          src="https://plausible.io/js/script.js"
+        ></script>
       </body>
     </html>
   );
--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,4 +1,4 @@
-export default function Error() {
+export default function ErrorPage() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center space-y-4">
       <h1 className="text-xl font-bold">An error occurred</h1>
