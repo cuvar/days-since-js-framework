bd1235f71d039a868d653242d23f61b9a41af9f6
cuvar <luca@cuvar.dev>
2024-08-18 11:47:19 +0200

    fix: wrong tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,7 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 } satisfies Config;
