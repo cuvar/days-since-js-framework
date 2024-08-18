23452a83a387dfdde805ce5c4150908721cadcf5
cuvar <luca@cuvar.dev>
2024-08-18 11:48:32 +0200

    fix: meta information in layout

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import ErrorPage from "~/components/Error";
 import HistoryPage from "~/components/History";
 import Loading from "~/components/Loading";
@@ -11,26 +10,12 @@ export default function Home() {
   const getCounterQuery = api.framework.getHistory.useQuery();
 
   return (
-    <>
-      <Head>
-        <title>Latest JS framework releases</title>
-        <meta
-          name="description"
-          content="History of JavaScript frameworks and their release dates."
-        />
-        <meta
-          name="keywords"
-          content="javaScript, framework, counter, last, js, days, since, release"
-        />
-      </Head>
-
-      <div className="flex h-full w-full flex-col items-center space-y-8 px-8">
-        {getCounterQuery.isLoading && <Loading />}
-        {getCounterQuery.isError && <ErrorPage />}
-        {getCounterQuery.isSuccess && (
-          <HistoryPage frameworks={getCounterQuery.data} />
-        )}
-      </div>
-    </>
+    <div className="flex h-full w-full flex-col items-center space-y-8 px-8">
+      {getCounterQuery.isLoading && <Loading />}
+      {getCounterQuery.isError && <ErrorPage />}
+      {getCounterQuery.isSuccess && (
+        <HistoryPage frameworks={getCounterQuery.data} />
+      )}
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import Counter from "~/components/Counter";
 import ErrorPage from "~/components/Error";
 import Loading from "~/components/Loading";
@@ -11,23 +10,10 @@ export default function Home() {
   const getCounterQuery = api.framework.getCounter.useQuery();
 
   return (
-    <>
-      <Head>
-        <title>Days since last JS framework</title>
-        <meta
-          name="description"
-          content="Counter for the days since the release of the latest JavaScript framework"
-        />
-        <meta
-          name="keywords"
-          content="javaScript, framework, counter, last, js, days, since, release"
-        />
-      </Head>
-      <div className="flex h-full w-full items-center justify-center">
-        {getCounterQuery.isLoading && <Loading />}
-        {getCounterQuery.isError && <ErrorPage />}
-        {getCounterQuery.isSuccess && <Counter {...getCounterQuery.data} />}
-      </div>
-    </>
+    <div className="flex h-full w-full items-center justify-center">
+      {getCounterQuery.isLoading && <Loading />}
+      {getCounterQuery.isError && <ErrorPage />}
+      {getCounterQuery.isSuccess && <Counter {...getCounterQuery.data} />}
+    </div>
   );
 }
