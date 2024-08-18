ff5a0c0b95ba7031888d59337389e70941ded7bc
cuvar <luca@cuvar.dev>
2024-08-18 11:04:29 +0200

    feat: rewrite api

--- src/routes/api/history/+server.ts
+++
@@ -1,7 +0,0 @@
-import { getHistoryData } from "$lib/server/query/history";
-import type { RequestHandler } from "./$types";
-
-export const GET: RequestHandler = async ({ url }) => {
-  const counterData = await getHistoryData();
-  return new Response(JSON.stringify(counterData));
-};
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,31 +1,14 @@
-import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { getCounterData } from "~/server/query/counter";
+import { getHistoryData } from "~/server/query/history";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
-  getLatest: publicProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
-
-    return post ?? null;
+  getCounter: publicProcedure.query(async () => {
+    const counterData = await getCounterData();
+    return counterData;
+  }),
+  getHistory: publicProcedure.query(async () => {
+    const counterData = await getHistoryData();
+    return counterData;
   }),
 });
